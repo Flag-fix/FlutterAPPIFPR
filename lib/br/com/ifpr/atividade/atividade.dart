c58666df9d20ceb5bda2bcb09e6f133248f73288
Flag-fix <isac.bandeir4@gmail.com>
2021-05-19 20:11:01 -0300

    revisão - variáveis e estrutura de decisão

---
+++ lib/br/com/ifpr/atividade/atividade.dart
@@ -0,0 +1,31 @@
+
+
+void main() {
+
+  String nome = "Informe seu Nome";
+  String cargo = "Informe seu cargo";
+
+  var qqCoisa = "Isso é um Texto"; //String
+  var qqCoisa1 = 1;
+
+  int num = 50;
+
+  if(num > 10){
+    print("Esse número é maior q 10");
+  }else{
+    print("Esse número é menor q 10");
+  }
+
+  if(qqCoisa1 == qqCoisa){
+    print("É igual");
+  }
+
+  List<String> acessos;
+
+  Map<String, dynamic> Mapzin()=> {
+    'acessos': acessos.map
+  };
+
+  Mapzin();
+
+}
