476a61edf7c109a7d2dacfabbc47e3beb716be86
Isac Bandeira <isac.bandeir4@gmail.com>
2021-07-09 18:21:00 -0300

    exclusão de item

--- lib/br.com.ifpr.atividade/app/view/pessoa_list.dart
+++ lib/br.com.ifpr.atividade/app/view/pessoa_list.dart
@@ -1,6 +1,9 @@
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:ifpr_flutter/br.com.ifpr.atividade/app/database/dataBaseConnection.dart';
+import 'package:ifpr_flutter/br.com.ifpr.atividade/app/domain/model/newPessoa.dart';
 import 'package:ifpr_flutter/br.com.ifpr.atividade/app/domain/model/pessoa.dart';
 import 'package:flutter/material.dart';
+import 'package:ifpr_flutter/br.com.ifpr.atividade/pessoa_list_back.dart';
 
 class PessoaList extends StatefulWidget {
   @override
@@ -8,56 +11,96 @@ class PessoaList extends StatefulWidget {
 }
 
 class _PessoaListState extends State<PessoaList> {
-  var _list = [];
 
-  Future pegarLista() async{
-    var resultado = await DatabaseApp.instance.buscarTodasPessoas();
-    setState(() {
-      _list = resultado;
-    });
+  final _back = PessoaListBack();
+
+  CircleAvatar avatar(String url){
+    try{
+      return CircleAvatar(backgroundImage: NetworkImage(url));
+    }catch(e){
+      return CircleAvatar(child: Icon(Icons.person));
+    }
   }
 
-  @override
-  void initState() {
-    pegarLista();
-    super.initState();
+  Widget editar(Function editar){
+    return IconButton(onPressed: editar, icon: Icon(Icons.edit),color: Colors.deepOrange);
   }
+
+  Widget remover(BuildContext context, Function remove){
+    return IconButton(
+      icon: Icon(Icons.delete),
+        color: Colors.red,
+        onPressed: (){
+        showDialog(context: context,
+            builder: (context)=>AlertDialog(
+              title: Text("Excluir"),
+              content: Text("Confirma Exclusão?"),
+              actions: [
+                FlatButton(onPressed: (){
+                  Navigator.of(context).pop();
+                }, child: Text("Não")),
+                FlatButton(onPressed: remove, child: Text("Sim")),
+              ],
+            ));
+        },
+
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-              appBar: AppBar(
-                title: Text('Contatos'),
-                actions: [
-                  IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () async{
-                        await Navigator.of(context).pushNamed('form');
-                        await pegarLista();
-                      })
-                ],
-              ),
-              body: ListView.separated(
-                separatorBuilder: (context, index)=> Divider(color: Colors.black,),
-                padding: const EdgeInsets.all(2.0),
-                itemCount: _list.length,
-                itemBuilder: (context, i) {
-                  final avatar = _list[i].foto.isEmpty  ? CircleAvatar(child: Icon(Icons.person)) : CircleAvatar(  backgroundImage: NetworkImage(_list[i].foto));
-                  return ListTile(
-                    leading: avatar,
-                    title: Text(_list[i].nome, style: TextStyle(fontWeight: FontWeight.bold,),),
-                    subtitle: Text('Ref-'+_list[i].referencia),
-                    trailing: Container(
-                      width: 100,
-                      child: Row(
-                        children: [
-                          IconButton(icon: Icon(Icons.edit), onPressed: null),
-                          IconButton(icon: Icon(Icons.delete), onPressed: null),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Contatos'),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () async {
+                  await Navigator.of(context).pushNamed('form');
+                })
+          ],
+        ),
+        body: Observer(
+          builder: (context) {
+            return FutureBuilder(
+                future: _back.lista,
+                builder: (context, index) {
+                  if (!index.hasData) {
+                    return CircularProgressIndicator();
+                  } else {
+                    List<NewPessoa> _list = index.data;
+                    return ListView.builder(
+                      padding: const EdgeInsets.all(2.0),
+                      itemCount: _list.length,
+                      itemBuilder: (context, i) {
+                        return ListTile(
+                          leading: avatar(_list[i].foto),
+                          title: Text(
+                            _list[i].nome,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text('Ref-' + _list[i].referencia),
+                          trailing: Container(
+                            width: 100,
+                            child: Row(
+                              children: [
+                                editar((){_back.goToForm(context,_list[i]);}),
+                                remover(context,(){
+                                  _back.remove(_list[i].id);
+                                  Navigator.of(context).pop();
+                                }),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                });
+          },
+        ));
   }
 }
